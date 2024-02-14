c0f14f0008c2b904b36a6acb163424864fdfe126
hendsalah25 <121400530+hendsalah25@users.noreply.github.com>
2024-02-14 18:47:23 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+function imgSlide(anything){
+    document.querySelector(".pepsi").src = anything;
+}
+function onchangBg(color){
+    const sec = document.querySelector(".sec");
+    sec.style.background = color;
+}
